8f2f8198cd2066986335ff64135403caa5ce89b2
mmorley-hl <77000005+mmorley-hl@users.noreply.github.com>
2021-01-07 12:35:05 +0100

    updated

--- .eleventy.js
+++ .eleventy.js
@@ -80,6 +80,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("static/img");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("_includes/assets/");
+  eleventyConfig.addPassthroughCopy("src/assets");
 
   /* Markdown Plugins */
   let markdownIt = require("markdown-it");
